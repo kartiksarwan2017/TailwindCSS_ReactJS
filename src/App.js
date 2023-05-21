cce0cc3568c1f4d2be56eacc54bc193346fa0a8a
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-05-21 11:02:03 +0530

    Using Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-8">
+        <div className="shrink-0">
+          <img className="h-12 w-12" src="./logo192.png" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 }
